cf7873035b6dda7bcbf222f2d33eed75615d4627
eliezerbuczak <eliezerbuczak2016@gmail.com>
2022-11-19 14:03:28 -0300

    Send notification

--- frontend/src/components/SalesCard/index.tsx
+++ frontend/src/components/SalesCard/index.tsx
@@ -13,11 +13,18 @@ function SalesCard() {
 
     const [sales, setSales] = useState<Sale[]>([]);
 
+
+
     useEffect(() => {
-        axios.get(`${BASE_URL}/sales`).then(response => {
+
+        const dMin = minDate.toISOString().slice(0, 10);
+        const dMax = maxDate.toISOString().slice(0, 10);
+        axios.get(`${BASE_URL}/sales?minDate=${dMin}&maxDate=${dMax}`).then(response => {
             setSales(response.data.content);
         })
-    }, [])
+    }, [minDate, maxDate])
+
+
     return (
 
         <div className="dsmeta-card">
@@ -67,7 +74,7 @@ function SalesCard() {
                                         <td>R$ {sale.amount.toFixed(2)}</td>
                                         <td>
                                             <div className="dsmeta-red-btn-container">
-                                                <NotificationButton />
+                                                <NotificationButton saleId={sale.id} />
                                             </div>
                                         </td>
                                     </tr>
